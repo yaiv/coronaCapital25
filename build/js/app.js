5dc70b328e934488f9ea8d0c67258266a81da929
yaiv <yaiv.gm@gmail.com>
2025-04-22 18:54:08 -0600

    Se comienza galeria

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,14 @@
-console.log('Prueba de consola');
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria;
+})
+
+function crearGaleria(){
+
+    const galeria = document.querySelector('.galeria-imagenes') //se inyecta en esta clase 
+
+    for (let i = 1; i <= 18; i++){
+        const imagen = document.createElement('IMG')
+        console.log(imagen);
+    }
+
+}
