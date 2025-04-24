5e069059b4270c5c604b98a8be4242e7985b6e1e
yaiv <yaiv.gm@gmail.com>
2025-04-24 17:40:41 -0600

    Se generan Tickets, falta mapa

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function(){
 function crearGaleria(){
 
     const galeria = document.querySelector('.galeria-imagenes') //se inyecta en esta clase 
-    const CANTIDAD_IMAGENES = 18
+    const CANTIDAD_IMAGENES = 20
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++){  //itera hasta que se llegue a las 18 imagenes de la galeria 
         const imagen = document.createElement('IMG') //Se genera la etiqueta de img
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = `Imagen de galeria ${i}`
+        imagen.src = `src/img/gallery/full/${i}.jpg` //Se pone la ruta de donde se va a trabajar con la imagen 
+        imagen.alt = `Imagen de galeria ${i}` //se integra texto de la galeria 
         
         galeria.appendChild(imagen)
 
